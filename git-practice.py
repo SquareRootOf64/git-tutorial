f50b0de1631f2d11c6fdc1671c262466d6be4d37
Olga Lavrukhina <olgalavrukhina@MacBook-Pro-Olga.local>
2023-08-22 18:57:21 +0300

    Version 3

--- git-practice.py
+++ git-practice.py
@@ -1,2 +1,2 @@
 print('Hello, World! It\'s the 1.0 Version \nLet\'s practice git!')
-print('Version 1.0')
+print('Version 3.0 ')
--- src/some-code.py
+++ src/some-code.py
@@ -1,1 +1,2 @@
-print('Hello World')
+print('Hello beautiful World', sum([2, 4, 6]))
+print('Version 3')
